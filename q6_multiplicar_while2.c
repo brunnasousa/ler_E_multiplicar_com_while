3232c26f9ddb25e74ee1636aaba8a2b2ebe84fe7
Brunna Sousa <brunnad68@gmail.com>
2022-05-23 20:20:19 -0300

    projeto final

--- q6_multiplicar_while2.c
+++ q6_multiplicar_while2.c
@@ -7,21 +7,21 @@ imprima em tela a multiplicação dos números.
 
 int main () {
 
-    int i = 1, armazenamentoUsuario  = 1, valorUsuario;
+    int i = 1, armazenamentoValorUsuario  = 1, valorUsuario;
 
     while (i<=10){
         printf("Digite o %d valor: " , i);
         scanf("%d", &valorUsuario);
         
-        armazenamentoUsuario *= valorUsuario;
+        armazenamentoValorUsuario *= valorUsuario;
 
         i++;
-
-        printf("--- AQUI: %d \n", armazenamentoUsuario);
     }
 
-    printf("Este e o resultado do armazenamento: %d", armazenamentoUsuario);
+    printf("Este e o resultado da multiplicação: %d", armazenamentoValorUsuario);
 
     return 0;
+
+    //Feito em 21/05/2022 By: brunnasousa e imxder (github)
 }
 
